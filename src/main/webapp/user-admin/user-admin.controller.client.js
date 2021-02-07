a7c5284e827519be6b57645aec3ef94cc414046d
zoey0_00 <zxy2183@gmail.com>
2021-02-07 14:53:43 -0500

    Update

---
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -0,0 +1,41 @@
+(function (){
+    var $usernameFld, $passwordfLD;
+    var $removeBtn, $editBtn, $createBtn;
+    var $firstNameFld, $lastNameFld, $roleFld;
+    var $userRowTemplate, $tbody;
+    var userRowTemplate, $tbody;
+    var userService = new AdminUserServiceClient();
+
+    $(main);
+    function main(){
+
+    }
+
+    function createUser(){
+
+    }
+
+    function findAllUsers(){
+
+    }
+
+    function findUserById(){
+
+    }
+
+    function deleteUser(){
+
+    }
+
+    function selectUser(){
+
+    }
+
+    function updateUser(){
+
+    }
+
+    function renderUsers(users){
+
+    }
+})
---
+++ src/main/webapp/user-admin/user.service.client.js
@@ -0,0 +1,26 @@
+function AdminUserServiceClient(){
+    this.createUser=creatreUser;
+    this.findAllUser=findAllUsers;
+    this.findUserById=finduSERbYiD;
+    this.deleteUser=deleteUser;
+    this.updateUser=updateUser;
+    this.url='https://wbdv-generic-server.herokuapp.com/api/001053190/users';
+    var self=this;
+    function createUser(user){
+
+    }
+    function findAllUsers(){
+
+    }
+
+    function findUserById(userId){
+
+    }
+
+    function updateUser(userId,user){
+
+    }
+    function deleteUser(userId){
+
+    }
+}
