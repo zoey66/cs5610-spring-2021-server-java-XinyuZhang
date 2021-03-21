b45e696a1a0012b58121b3351ff3808292db3661
zoey0_00 <>
2021-03-21 01:12:47 -0400

    A5_server

---
+++ src/main/java/com/example/wbdvspring202101xinyuserverjava/controllers/WidgetController.java
@@ -0,0 +1,55 @@
+package com.example.wbdvspring202101xinyuserverjava.controllers;
+
+import com.example.wbdvspring202101xinyuserverjava.models.Widget;
+import com.example.wbdvspring202101xinyuserverjava.services.WidgetService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class WidgetController {
+    @Autowired
+    WidgetService service;// = new WidgetService();
+
+    @PostMapping("/api/topics/{tid}/widgets")
+    public Widget createWidgetForTopic(
+            @PathVariable("tid") String topicId,
+            @RequestBody Widget widget) {
+        widget.setTopicId(topicId);
+        return service.createWidgetForTopic(widget);
+    }
+
+    @GetMapping("/api/topics/{tid}/widgets")
+    public List<Widget> findWidgetsForTopic(
+            @PathVariable("tid") String topicId
+    ) {
+        return service.findWidgetsForTopic(topicId);
+    }
+
+    @GetMapping("/api/widgets")
+    public List<Widget> findAllWidgets() {
+        return service.findAllWidgets();
+    }
+
+    @GetMapping("/api/widgets/{wid}")
+    public Widget findWidgetById(
+            @PathVariable("wid") Long id) {
+        return service.findWidgetById(id);
+    }
+
+    @DeleteMapping("/api/widgets/{wid}")
+    public Integer deleteWidget(@PathVariable("wid") Long id) {
+        return service.deleteWidget(id);
+    }
+
+    @PutMapping("/api/widgets/{wid}")
+    public Integer updateWidget(
+            @PathVariable("wid") Long id,
+            @RequestBody Widget widget) {
+        return service.updateWidget(id, widget);
+    }
+}
---
+++ src/main/java/com/example/wbdvspring202101xinyuserverjava/services/WidgetService.java
@@ -0,0 +1,74 @@
+package com.example.wbdvspring202101xinyuserverjava.services;
+
+import org.springframework.stereotype.Service;
+import com.example.wbdvspring202101xinyuserverjava.models.Widget;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class WidgetService {
+    private List<Widget> widgets = new ArrayList<Widget>();
+    {
+        Widget w1 = new Widget(123l, "ABC123", "HEADING", 1, "Welcome to Widgets");
+        Widget w2 = new Widget(234l, "ABC234", "PARAGRAPH", 1, "This is a paragraph");
+        Widget w3 = new Widget(345l, "ABC234", "HEADING", 2, "Welcome to WebDev");
+        Widget w4 = new Widget(456l, "ABC234", "PARAGRAPH", 1, "Lorem ipsum");
+        widgets.add(w1);
+        widgets.add(w2);
+        widgets.add(w3);
+        widgets.add(w4);
+    }
+    // implement crud operations
+    public Widget createWidgetForTopic(Widget widget) {
+        Long id = (new Date()).getTime();
+        widget.setId(id);
+        widgets.add(widget);
+        return widget;
+    }
+    public List<Widget> findAllWidgets() {
+        return widgets;
+    }
+    public List<Widget> findWidgetsForTopic(String topicId) {
+        List<Widget> ws = new ArrayList<Widget>();
+        for(Widget w: widgets) {
+            if(w.getTopicId().equals(topicId)) {
+                ws.add(w);
+            }
+        }
+        return ws;
+    }
+    public Widget findWidgetById(Long id) {
+        for(Widget w: widgets) {
+            if(w.getId().equals(id)) {
+                return w;
+            }
+        }
+        return null;
+    }
+    public Integer updateWidget(Long id, Widget newWidget) {
+        for(int i=0; i<widgets.size(); i++) {
+            Widget w = widgets.get(i);
+            if(w.getId().equals(id)) {
+                widgets.set(i, newWidget);
+                return 1;
+            }
+        }
+        return -1;
+    }
+    public Integer deleteWidget(Long id) {
+        int index = -1;
+        for(int i=0; i<widgets.size(); i++) {
+            Widget w = widgets.get(i);
+            if(w.getId().equals(id)) {
+                index = i;
+            }
+        }
+        if(index >= 0) {
+            widgets.remove(index);
+            return 1;
+        }
+        return -1;
+    }
+}
